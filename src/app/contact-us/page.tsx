ad8e1a897152649bee0429234a18c1487f5ecc7c
meadarsh <itsadarsh33@gmail.com>
2024-12-03 09:39:10 +0530

    update

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import HeroSection1 from './../components/HeroSection1';
+import IconsSection2 from './../components/IconsSection2';
+import GetInTouchSection3 from './../components/GetInTouchSection3';
+import OurOfficesSection4 from './../components/OurOfficesSection4';
 
 const page = () => {
   return (
-    <div>page</div>
+    <>
+      <HeroSection1 />
+      <IconsSection2 />
+      <GetInTouchSection3 />
+      <OurOfficesSection4 />
+      
+    </>
   )
 }
 
