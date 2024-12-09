09f2e48004b73d45319b474d1d818a8d4d17fc5e
meadarsh <itsadarsh33@gmail.com>
2024-12-10 00:50:35 +0530

    Api add

---
+++ src/app/api/send-mail/route.ts
@@ -0,0 +1,18 @@
+import nodemailer from "nodemailer";
+import { NextResponse } from "next/server";
+
+export async function POST(request: any) {
+  try {
+    const data = await request.json();
+
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "myhrexpert19@gmail.com",
+        pass: "wyub kmpl fzrm dvxs",
+      },
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
