f138c9ce068ab006d070d63bd5d9587ade078c8a
meadarsh <itsadarsh33@gmail.com>
2024-12-10 10:54:29 +0530

    Port fix

--- ecosystem.config.ts
+++ ecosystem.config.ts
@@ -4,9 +4,10 @@ module.exports = {
         name: "auth-app",
         script: "npm",
         args: "start",
+        interpreter: "ts-node", // Use ts-node to run the TypeScript file
         env: {
           NODE_ENV: "production",
-          PORT: 4222, // Define your custom port here
+          PORT: 4222,
         },
       },
     ],
