3a9380aa4d0e3532a213123eb03ad6f467604f5e
meadarsh <itsadarsh33@gmail.com>
2024-12-14 11:40:42 +0530

    fix

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,13 +1,14 @@
+"use client"
 import React from "react";
 import BlogPage from "./component/BlogPage";
 
-export const metadata = {
-  title: "Latest blogs for better guidance - College Counsel",
-  description: "Get admission in your dream University and course through College Counsel and get expert counselling and guidance along with scholarship options.",
-  alternates: {
-    canonical:`${process.env.BASE_URL}/blog`,
-  },
-};
+// export const metadata = {
+//   title: "Latest blogs for better guidance - College Counsel",
+//   description: "Get admission in your dream University and course through College Counsel and get expert counselling and guidance along with scholarship options.",
+//   alternates: {
+//     canonical:`${process.env.BASE_URL}/blog`,
+//   },
+// };
 
 const page = () => {
  
