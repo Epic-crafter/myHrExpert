ce8464dea1e2e712326997e264214fbb3cd3b0de
Delta Student <kushkumar53021@gmail.com>
2025-01-02 13:55:09 +0530

    responsivness added

--- src/app/components/HeroSection1.tsx
+++ src/app/components/HeroSection1.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 import ContactForm from './GetInTouchSection3'
 const HeroSection1 = () => {
   return (
-    <div className="bg-[#000D51] min-h-screen flex flex-col md:flex-row gap-20 md:gap-0">
-    <div className="section-1 flex-1 relative p-6 md:p-12">
-      <div className="hero-text-btn absolute w-full md:w-[474px] gap-[40px] top-[231px] left-0 md:left-auto mx-auto md:mx-0 text-start">
+    <div className="bg-[#000D51] flex flex-col md:flex-row">
+    <div className="section-1 flex justify-center items-center p-6 md:p-12">
+      <div className="hero-text-btn gap-[40px] mx-auto md:mx-0 text-center">
         {/* <span className="text-[#FFFFFFCC] text-sm">Contact us</span> */}
-        <h1 className="text-white text-3xl md:text-4xl font-bold mt-2 w-full md:w-[474px]">
+        <h1 className="text-white text-3xl md:text-4xl font-bold mt-2">
         "Have HR Challenges? We’ve Got Solutions!"
         </h1>
-        <p className="text-[#FFFFFFCC] text-xs md:text-sm mt-2 w-full md:w-[474px]">
+        <p className="text-[#FFFFFFCC] text-xs md:text-sm mt-2">
         Whether it’s a query, a partnership, or feedback, we’re all ears.
         </p>
         {/* <div className="mt-4 px-4 md:px-[0]">
@@ -21,8 +21,8 @@ const HeroSection1 = () => {
     </div>
   
 
-    <div className="section-2 hidden md:flex relative h-auto top-0 md:top-0 flex-1 justify-center items-center p-6 md:p-12 right-12">
-      <div className="relative w-full max-w-lg">
+    <div className="section-2 flex flex-col md:flex-row relative h-auto p-6 md:p-12 w-full md:w-1/2">
+      <div className="w-full">
         <ContactForm></ContactForm>
       </div>
     </div>
